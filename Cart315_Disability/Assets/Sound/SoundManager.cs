8bc184f992e57193bd5b40eb9b50f95c8088955d
Nadia-Abdul-Aziz <nadiaabdulaziz01@gmail.com>
2026-04-09 16:59:27 -0400

    script

--- Cart315_Disability/Assets/Sound/SoundManager.cs
+++ Cart315_Disability/Assets/Sound/SoundManager.cs
@@ -1,16 +1,41 @@
 using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine;
+public enum SoundType
+{
 
+Walk1,
+Walk2,
+Jump,
+Click,
+Interact,
+Dialogue,
+
+}
 public class SoundManager : MonoBehaviour
 {
+    [SerializeField] private AudioClip[] soundList;
+    private static SoundManager instance;
+    private AudioSource audioSource;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Awake()
     {
-        
+        instance = this;
+    
+    }
+
+    private void Start()
+    {
+    audioSource = GetComponent<AudioSource>();
+
     }
 
     // Update is called once per frame
-    void Update()
+    public static void PlaySound(SoundType sound, float volume = 1)
     {
-        
+    
+    instance.audioSource.PlayOneShot(instance.soundlist[(int)sound], volume);
+
     }
 }
